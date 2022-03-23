30745c803e08031421046dfbcb35fd28c9b55e48
Ryo-Ura <urawaryosuke@gmail.com>
2022-03-22 17:32:08 -0700

    add alert

--- page/main.js
+++ page/main.js
@@ -1,6 +1,7 @@
 const initButton = () => {
     const submitButton = document.getElementById("submit-button");
     submitButton.innerHTML = "SUBMITTED";
+    submitButton.style.color = "blue";
 }
 
 initButton();
